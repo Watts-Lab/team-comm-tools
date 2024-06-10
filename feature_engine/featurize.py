ff464a4a6f66e4cb7d598b6de6d358efe9cca067
agshruti12 <agshruti2901@gmail.com>
2024-06-09 23:26:23 -0400

    fix featurizer

--- feature_engine/featurize.py
+++ feature_engine/featurize.py
@@ -85,23 +85,34 @@ if __name__ == "__main__":
 		input_df = chat_df,
 		ner_training_df = test_ner_training_df,
 		vector_directory = "../feature_engine/tpm-data/vector_data/",
+		output_file_path_chat_level = "../feature_engine/output/chat/test_chat_level_chat.csv",
+		output_file_path_user_level = "../feature_engine/output/user/test_chat_level_user.csv",
+		output_file_path_conv_level = "../feature_engine/output/conv/test_chat_level_conv.csv",
+		turns = False,
+	)
+	testing_chat.featurize(col="message")
+
+	# testing perturbed chat level features
+	testing_chat_complex = FeatureBuilder(
+		input_df = chat_complex_df,
+		vector_directory = "../feature_engine/tpm-data/vector_data/",
 		output_file_path_chat_level = "../feature_engine/output/chat/test_chat_level_chat_complex.csv",
 		output_file_path_user_level = "../feature_engine/output/user/test_chat_level_user_complex.csv",
 		output_file_path_conv_level = "../feature_engine/output/conv/test_chat_level_conv_complex.csv",
 		turns = False,
 	)
-	testing_chat_complex.featurize(col="message")
+	testing_chat.featurize(col="message")
 
 	# testing conv features
-	# testing_conv = FeatureBuilder(
-	# 	input_df = conv_df,
-	# 	vector_directory = "../feature_engine/tpm-data/vector_data/",
-	# 	output_file_path_chat_level = "../feature_engine/output/chat/test_conv_level_chat.csv",
-	# 	output_file_path_user_level = "../feature_engine/output/user/test_conv_level_user.csv",
-	# 	output_file_path_conv_level = "../feature_engine/output/conv/test_conv_level_conv.csv",
-	# 	turns = False,
-	# )
-	# testing_conv.featurize(col="message")
+	testing_conv = FeatureBuilder(
+		input_df = conv_df,
+		vector_directory = "../feature_engine/tpm-data/vector_data/",
+		output_file_path_chat_level = "../feature_engine/output/chat/test_conv_level_chat.csv",
+		output_file_path_user_level = "../feature_engine/output/user/test_conv_level_user.csv",
+		output_file_path_conv_level = "../feature_engine/output/conv/test_conv_level_conv.csv",
+		turns = False,
+	)
+	testing_conv.featurize(col="message")
 
 	# FULL DATASETS BELOW ------------------------------------- #
 	
